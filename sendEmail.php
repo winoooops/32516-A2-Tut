b41f6994fb7c0e02294125dbd75d4ea9936f9e04
winoooops <winoooops@outlook.com>
2020-05-30 18:52:55 +1000

    update to the end of the 2nd tut

---
+++ sendEmail.php
@@ -0,0 +1,17 @@
+<?php
+  // get the thing from sessionStorage to PHP and sent it as part of the email  
+  if(isset($_POST['submit']) && isset($_POST['emailAddress']) ) {
+    $cars = $_POST['carInfo'];
+    echo $cars;
+    $msg = "a email has been sent to you";
+    // $msg += $cars;
+    $email = $_POST['emailAddress'];
+
+  }
+  
+    if( mail($email, 'an order', $msg)) {
+      echo "email success"; 
+    }  
+  
+  
+?>
